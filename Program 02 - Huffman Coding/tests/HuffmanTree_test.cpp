18125c5534197e7bc50fe2f06333c7a47288faf2
Tamara Roberson <tamara.roberson@gmail.com>
2020-02-01 07:05:11 -0800

    (Lab 4 - Huffman) Enable tests

--- Program 02 - Huffman Coding/tests/HuffmanTree_test.cpp
+++ Program 02 - Huffman Coding/tests/HuffmanTree_test.cpp
@@ -5,3 +5,27 @@
 //
 // Description: A compression algorithm using Huffman encoding
 ////
+
+#include "HuffmanTree.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <catch2/catch.hpp>
+
+
+SCENARIO("HuffmanTree: Build Tree finds the most frequent character")
+{
+    GIVEN("The string 'Hello, World!'")
+    {
+        HuffmanTree tree{"Hello, World!"};
+
+        THEN("The most common letter is 'l'")
+        {
+            std::stringstream result{};
+            tree.printTree(result);
+            REQUIRE(result.str() == "l");
+        }
+    }
+}
